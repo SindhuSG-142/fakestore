c04c9a2dfa0690f5b1ed69b9fa35be8d5983f56a
unknown <sgsindhusrinivas@gmail.com>
2024-04-27 22:26:17 +0530

    code to get product by category

--- product.js
+++ product.js
@@ -3,13 +3,19 @@ const dataDisplay=document.getElementById('container');
 fetch('https://fakestoreapi.com/products/categories')
             .then(res=>res.json())
             .then(json=>console.log(json))
+fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+
 function displayProduct(products){
     dataDisplay.innerHTML=""
-    item.forEach((product)=>{
+    products.forEach((product)=>{
         dataDisplay.innerHTML+=`
         <div class="product-card">
         <img class="product-pic" src="${product.pic}" alt="">
         <div class="title">${product.title}</div>
-        <div class="product-price">${product.price}></div>`
+        <div class="product-price">${product.price}></div>
+        </div>`
     })
 }
